cfa3be81eaf8799098d90d0669f6eb15e88cc5b3
nguye <nguyenv2@outlook.com>
2019-01-07 15:17:41 -0800

    octave updated calculations

--- Octave/Estimator.m
+++ Octave/Estimator.m
@@ -1,15 +1,38 @@
 clear all
 close all
 
-formatArray = [2,16384]
-file301 = fopen('../assets/301.sc')
-data301 = fread(file301, formatArray, 'int16')
+formatArray = [2,16384];
+file301 = fopen('../assets/301.sc');
+file303 = fopen('../assets/303.sc');
+data301 = fread(file301, formatArray, 'int16');
+data303 = fread(file303, formatArray, 'int16');
+barker13 = [1,1,1,1,1,-1,-1,1,1,-1,1,-1,1];
 
-IQ301 = data301(1,:) + i*data301(2,:)
-IQ301Reshape = mean(transpose(reshape(IQ301,256,64)),2)
+function x = BarkerEstimator(data, barker)
+  reshapeData = data(1,:) + i*data(2,:);
+  complex = transpose(reshape(reshapeData,64,256));
+  for i=1:1:256
+    correlateData(i,:) = conv(fliplr(barker),complex(i,:));
+  end
+  Power = abs(correlateData).^2;
+  figure
+  plot (mean(Power))
+end
 
-barker13 = [1,1,1,1,1,-1,-1,1,1,-1,1,-1,1]
+function y = PulseEstimator(data)
+  reshapeData = data(1,:) + i*data(2,:);
+  complex = transpose(reshape(reshapeData,64,256));
+  Power = abs(complex).^2;
+  figure
+  plot (mean(Power))
+end
 
-figure
-plot (abs(IQ301Reshape).^2)
+function a = FFTPuse(data)
+  reshapeData = data301(1,:) + i*data301(2,:);
+  complex = transpose(reshape(reshapeData,64,256));
+  FFT = fft(complex,[],1)
+  plot(fftshift(abs(FFT)))
+end
 
+PulseEstimator(data301)
+BarkerEstimator(data303, barker13)
---
+++ README.md
@@ -0,0 +1,9 @@
+# Pulse Estimator
+
+Work in progress....
+
+## Octave Output
+
+![console](assets/pulsecorrelation.PNG)
+
+![console](assets/barkercorrelation.PNG)
